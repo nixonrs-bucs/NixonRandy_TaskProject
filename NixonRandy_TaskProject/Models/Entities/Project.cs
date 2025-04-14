be821d9a1444cc3909433ae88bbfa0258a716cfa
RandyNixon <79169426+nixonrs-bucs@users.noreply.github.com>
2025-04-14 11:01:01 -0400

    Second Commit
    I got both the project and task classes completed

---
+++ NixonRandy_TaskProject/Models/Entities/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace NixonRandy_TaskProject.Models.Entities;
+public class Project
+{
+   public int ProjectId { get; set; }
+   public string Title { get; set; } = string.Empty;
+   public string? Description { get; set; } = string.Empty;
+   public DateTime CreationDate { get; set; } = DateTime.Now;
+   public DateTime? DueDate { get; set; } = null;
+}
---
+++ NixonRandy_TaskProject/Models/Entities/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace NixonRandy_TaskProject.Models.Entities;
+public class Task : Project
+{
+    public int projectId { get; set; }
+    public string TaskTitle { get; set; } = string.Empty;
+    public string? TaskDescription { get; set; } = string.Empty;
+    public DateTime TaskCreationDate { get; set; } = DateTime.Now;
+    public DateTime? TaskDueDate { get; set; } = null;
+}
