62faae60122241824d548388b9552fbd7a6c0237
nixonrs-bucs <79169426+nixonrs-bucs@users.noreply.github.com>
2025-04-14 10:46:35 -0400

    First initial commit
    My first initial commit to the main repo

---
+++ NixonRandy_TaskProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NixonRandy_TaskProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
