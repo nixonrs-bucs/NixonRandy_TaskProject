2e500b41581b8c94d20e4c3b8fa786cd55d86f6a
RandyNixon <79169426+nixonrs-bucs@users.noreply.github.com>
2025-05-02 15:05:34 -0400

    Another Big release on CRRUD
    Finally got CRRUD done as of part of the project. Now, I'll have to get it running on the website for the next tasks.

--- NixonRandy_TaskProject/Data/ApplicationDbContext.cs
+++ NixonRandy_TaskProject/Data/ApplicationDbContext.cs
@@ -3,10 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace NixonRandy_TaskProject.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+
+/*public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
     }
-}
+}*/
