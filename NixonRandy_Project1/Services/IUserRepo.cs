eae59d6b3fb48b6e0192972bd9868e319c3222c8
nixonrs-bucs <79169426+nixonrs-bucs@users.noreply.github.com>
2025-05-04 20:14:15 -0400

    Delete NixonRandy_Project1 directory

--- NixonRandy_Project1/Services/IUserRepo.cs
+++
@@ -1,9 +0,0 @@
-﻿using NixonRandy_Project1.Models.Entities;
-namespace NixonRandy_Project1.Services;
-public interface IUserRepo
-{
-    public Task<User> DetailsAsync(int id);
-    public Task<User> RegisterAsync(User user);
-    public Task<User> LogoutAsync();
-    public Task<User> DeleteAsync(int id);
-}
